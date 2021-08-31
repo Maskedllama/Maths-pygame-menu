5ea989315a8772a4de82bcc2ddca57dfaab60a69
Maskedllama <87449678+Maskedllama@users.noreply.github.com>
2021-08-31 17:45:42 +0200

    Add files via upload

---
+++ Menu121.py
@@ -0,0 +1,66 @@
+import pygame
+import pygame_menu
+
+# Initialize pygame
+pygame.init()
+surface = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Projecte MatZanfe")
+
+
+font = pygame_menu.font.FONT_8BIT
+font1 = pygame_menu.font.FONT_NEVIS
+font2 = pygame_menu.font.FONT_BEBAS
+
+#Make menu Sumes
+menu3 = pygame_menu.Menu('Sums', 600, 400,
+                       theme=pygame_menu.themes.THEME_BLUE)
+
+menu3.add.button('Infinite sums', font_name = font1, font_color = 'green')
+menu3.add.button('Sums with images', font_name = font1, font_color = 'blue')
+menu3.add.button('Sums with audio', font_name = font1,font_color = 'red')
+
+#Make menu Restes
+menu4 = pygame_menu.Menu('Subtraction', 600, 400,
+                       theme=pygame_menu.themes.THEME_DEFAULT)
+
+menu4.add.button('Infinite subtractions', font_name = font1, font_color = 'green')
+menu4.add.button('Subtractions with images', font_name = font1, font_color = 'blue')
+menu4.add.button('Subtratcions with audio', font_name = font1,font_color = 'red')
+
+#Make menu Multiplicacions
+menu5 = pygame_menu.Menu('Multiplications', 600, 400,
+                       theme=pygame_menu.themes.THEME_ORANGE)
+
+menu5.add.button('Infinite multis', font_name = font1, font_color = 'green')
+menu5.add.button('Multis with images', font_name = font1, font_color = 'blue')
+menu5.add.button('Multis with audio', font_name = font1,font_color = 'red')
+
+#Make menu Divisions
+menu6 = pygame_menu.Menu('Divisions', 600, 400,
+                       theme=pygame_menu.themes.THEME_GREEN)
+
+menu6.add.button('Infinite divisions', font_name = font1, font_color = 'green')
+menu6.add.button('Divisions with images', font_name = font1, font_color = 'blue')
+menu6.add.button('Divisions with audio', font_name = font1,font_color = 'red')
+
+# Make menu 2
+menu2 = pygame_menu.Menu('Module selection', 600, 400,
+                       theme=pygame_menu.themes.THEME_SOLARIZED)
+
+
+menu2.add.button('Sums',menu3, font_name = font2, font_color = 'green')
+menu2.add.button('Subtractions',menu4, font_name = font2, font_color = 'blue')
+menu2.add.button('Multiplications',menu5, font_name = font2,font_color = 'red')
+menu2.add.button('Divisions',menu6, font_name = font2, font_color = 'purple' )
+
+# Make main menu
+menu = pygame_menu.Menu('Projecte MatZanfe', 600, 400,
+                       theme=pygame_menu.themes.THEME_SOLARIZED)
+
+user_input = menu.add.text_input('User: ', font_name = font1, font_color = 'blue')
+age_input = menu.add.text_input('Age: ', font_name = font1,font_color = 'Black')
+menu.add.button('Start', menu2, font_name = font, font_color = 'green')
+menu.add.button('Exit', pygame_menu.events.EXIT, font_name = font,font_color = 'red')
+
+# Run the menu
+menu.mainloop(surface)
